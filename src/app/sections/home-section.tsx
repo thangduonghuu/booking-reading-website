fe4d6decc40b2dced0f111c389d82333602ce9b6
Thang Duong <thang.duong@cyberlogitec.com>
2025-08-06 23:35:59 +0700

    update section page

---
+++ src/app/sections/home-section.tsx
@@ -0,0 +1,44 @@
+'use client';
+import Image from "next/image";
+import { useEffect } from "react";
+
+export default function HomeSection() {
+  useEffect(() => {
+    const sound = new Howl({
+      src: ['https://static.thangduong.info/public/background-chill.mp3'], // replace with your public path
+      autoplay: true,
+      loop: true,
+      volume: 1.0,
+      mute: true, // muted initially to allow autoplay
+      onplay: () => {
+        sound.mute(false) // unmute after it starts
+      },
+      onloaderror: (id, err) => {
+        console.error('Load error:', err)
+      },
+      onplayerror: (id, err) => {
+        console.warn('Autoplay failed. Waiting for user interaction...')
+        const resumeOnClick = () => {
+          sound.play()
+          window.removeEventListener('click', resumeOnClick)
+        }
+        window.addEventListener('click', resumeOnClick, { once: true })
+      }
+    })
+
+    return () => {
+      sound.unload()
+    }
+  }, [])
+  
+  return (
+    <div className='flex flex-col items-center justify-center h-screen absolute w-full'>
+      <Image
+        src="/images/home-book.png"
+        alt="Home"
+        fill
+        objectFit='contain'
+      />
+    </div>
+  );
+}
