eade7606d498f973b39788e70fb70b91ea120891
Thang Duong <thang.duong@cyberlogitec.com>
2025-08-09 12:49:29 +0700

    fix build domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ["static.thangduong.info"],
     remotePatterns: [
       {
         protocol: "https",
