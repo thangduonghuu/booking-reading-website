251f4e84f31880429b5e5e44422a2490161fe349
Thang Duong <thang.duong@cyberlogitec.com>
2025-08-09 17:15:36 +0700

    update home page paginate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
-import BackgroundMusic from "./sections/background-music";
-import { Music } from "../../public/icons";
-import Navbar from "./components/Navbar";
 import MusicProvider from "@/context/MusicContext";
+import Image from "next/image";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Navbar from "./components/Navbar";
+import PaginateHome from "./sections/paginate-home";
+// import './styles.css';
 
 
 export default function Home() {
@@ -11,20 +15,12 @@ export default function Home() {
       <div className='flex flex-col h-screen  '>
         <Navbar />
         <div className='flex flex-col items-center justify-center h-screen absolute w-full'>
-          <div className="relative w-full h-full flex items-center justify-center max-w-[675px] max-h-[500px]">
-            <Image
-              src="https://static.thangduong.info/public/home-book.png"
-              alt="Home"
-              fill
-              className="object-contain !left-1/2 !top-1/2 transform -translate-x-1/2 -translate-y-1/2"
-            />
-          </div>
+          <PaginateHome />
           <div className="max-w-[320px] text-center mt-8">
             <h1 className="text-2xl font-bold">Read your favourite books</h1>
             <h2 className="text-[16px] text-[#9D9D9D] pt-2">All your favourites book in one place, read any book, staying at home, on travelling, or anywhere else</h2>
           </div>
         </div>
-        <BackgroundMusic />
       </div>
     </MusicProvider>
   );
---
+++ src/app/sections/paginate-home.tsx
@@ -0,0 +1,52 @@
+'use client';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import Image from "next/image";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from "swiper/modules";
+
+
+export default function PaginateHome() {
+    return (
+        <div className="w-full flex items-center justify-center">
+            <Swiper
+                pagination={true} modules={[Pagination]} className="w-[675px] h-[500px]"
+            >
+                <SwiperSlide >
+                    <div className="relative w-full h-full flex items-center justify-center ">
+
+                        <Image
+                            src="/images/home-background-1.png"
+                            alt="Home"
+                            fill
+                            className="object-contain !left-1/2 !top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className="relative w-full h-full flex items-center justify-center ">
+
+                        <Image
+                            src="/images/home-background-2.png"
+                            alt="Home"
+                            fill
+                            className="object-contain !left-1/2 !top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <div className="relative w-full h-full flex items-center justify-center ">
+
+                        <Image
+                            src="/images/home-background-3.png"
+                            alt="Home"
+                            fill
+                            className="object-contain !left-1/2 !top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+}
