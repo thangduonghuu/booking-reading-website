d82423f54bfdb5067b8bfa25f7ae849dd47774e3
Thang Duong <thang.duong@cyberlogitec.com>
2025-10-11 21:50:33 +0700

    update Typography

--- src/app/components/CardBook/CardBook.tsx
+++ src/app/components/CardBook/CardBook.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function CardBook({ title, author, coverImage }: { title: string; author: string; coverImage: string }) {
@@ -7,8 +8,8 @@ export default function CardBook({ title, author, coverImage }: { title: string;
                 <Image src={coverImage} alt={`${title} cover`} className="cover-image rounded-[8px]" objectFit="cover" fill />
             </div>
             <div className="pt-2 w-[300px]">
-                <h3 className="text-[16px] font-bold">{title}</h3>
-                <p className="author text-[12px]">{author}</p>
+                <Typography fontSize={16} className=" font-bold">{title}</Typography>
+                <Typography className="author text-[12px]">{author}</Typography>
             </div>
         </div>
     );
